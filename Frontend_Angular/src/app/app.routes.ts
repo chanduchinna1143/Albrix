707c79eb921a455a77cdc05cb4ac5638c5325fc7
ChandrasekharGiri <chanduchinna1143@gmail.com>
2026-02-03 15:40:23 +0530

    Components Created & Routes working

--- Frontend_Angular/src/app/app.routes.ts
+++ Frontend_Angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Landing } from './pages/landing/landing';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
+import { Chat } from './pages/chat/chat';
+
+export const routes: Routes = [
+    {path:'',component:Landing},
+    {path:'login', component:Login},
+    {path:'signup',component:Signup},
+    {path:'chat',component:Chat},
+    {path:'**',redirectTo:''}
+
+];
